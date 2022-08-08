7fa4bb21969f82baf291ed9949fd95c083a36857
= <andylackk@outlook.com>
2022-08-08 11:27:12 -0500

    improved setSubmittedBirthDate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {useState,useEffect} from 'react'
 
 import { Routes, Route, useNavigate } from "react-router-dom";
 
@@ -14,33 +13,53 @@ function App() {
 
 const navigate =  useNavigate() 
 
-const [submittedBirthDate,setSubmittedBirthDate] = useState('')
+const setSubmittedBirthDate = ([month,date,year]) => {
 
-
-
-
-useEffect(()=>{
-const birthDateString = submittedBirthDate.toString()
-const birthDateDate = new Date(birthDateString.replace(',',' '))
-const birthDateTime = birthDateDate.getTime()
-
-const diff_ms = Date.now()-birthDateTime
-var age_dt = new Date(diff_ms);
-
-const age = Math.abs(age_dt.getUTCFullYear() - 1970);
-
-
-if(!isNaN(age)){
-  console.log('age is now a number', age)
-  if(age >= 21){
-    navigate('/drinks')
+  if(month === ''||date===''||year===''){
+    alert('please enter full birthdate')
   } else {
-    alert('You are not old enough to view the drinks page.')
+    const birthDateArray = [month,date,year]
+    const birthDateString = birthDateArray.toString()
+    const birthDateDate = new Date(birthDateString.replace(',',' '))
+    const birthDateTime = birthDateDate.getTime()
+    const diff_ms = Date.now()-birthDateTime
+    var age_dt = new Date(diff_ms);
+    const age = Math.abs(age_dt.getUTCFullYear() - 1970);
+    console.log(age)
+    if(age >= 21){
+      navigate('/drinks')
+      } else {
+        alert('You are not old enough to view the drinks page.')
+      }
   }
-} 
+  
+}
 
 
-},[submittedBirthDate])
+
+
+// useEffect(()=>{
+// const birthDateString = submittedBirthDate.toString()
+// const birthDateDate = new Date(birthDateString.replace(',',' '))
+// const birthDateTime = birthDateDate.getTime()
+
+// const diff_ms = Date.now()-birthDateTime
+// var age_dt = new Date(diff_ms);
+
+// const age = Math.abs(age_dt.getUTCFullYear() - 1970);
+
+
+// if(!isNaN(age)){
+//   console.log('age is now a number', age)
+//   if(age >= 21){
+//     navigate('/drinks')
+//   } else {
+//     alert('You are not old enough to view the drinks page.')
+//   }
+// } 
+
+
+// },[submittedBirthDate])
 
   return (
     <div className="App">
